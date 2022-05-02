50b6cdf8ead35dc0b2dfc1b51ac46d1e2d345afe
Sunjin <tjswls8062@skuniv.ac.kr>
2022-05-02 15:52:51 +0900

    intelliJ commit

---
+++ JAVA_study/src/cs_10773.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+import java.util.Scanner;
+
+public class cs_10773 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int K = in.nextInt();
+
+        for(int i = 0; i < K; i++) {
+            int number = in.nextInt();
+
+            if(number == 0) {	// 0이라면 스택에 저장된 top 원소를 지운다.
+                stack.pop();
+            }
+            else {
+                stack.push(number);
+            }
+        }
+        int sum = 0;
+
+        for(int o : stack) {
+            sum += o;
+        }
+
+        System.out.println(sum);
+    }
+
+}
