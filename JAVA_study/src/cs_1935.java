8542e50931e18096dda6ba5365b3e158ff78543d
Sunjin <tjswls8062@skuniv.ac.kr>
2022-06-02 17:09:28 +0900

    intelliJ commit

--- JAVA_study/src/cs_1935.java
+++ JAVA_study/src/cs_1935.java
@@ -14,14 +14,14 @@ public class cs_1935 {
         for(int i = 0; i < num; i++){
             array[i] = Integer.parseInt(br.readLine());
         }
-
+        // 1 2 3 4 5
         Stack<Double> stack = new Stack<Double>();
 
         for(int i = 0; i < str.length(); i++){
             char ch = str.charAt(i);
             if('A' <= ch && ch <= 'Z'){
-                double data = array[ch - 'A'];
-                stack.push(data);
+                double data = array[ch - 'A'];  // 피연산자는 순서대로 나오므로
+                stack.push(data);               // 스택에 쌓는 작업
             }
             else{
                 double data1 = stack.pop();
--- JAVA_study/src/cs_2437.java
+++ JAVA_study/src/cs_2437.java
@@ -13,28 +13,27 @@ public class cs_2437 {
         }
 
         Arrays.sort(weight);
-
         /*
-        1 2 5 이면
-        1 2 3   5 6 7  저울질 가능
-        sum = 3
-        answer : 4
+        1 2   3 이면
+        1 2 3 4 5 6    저울질 가능
+        sum = 6
+        answer : 7
 
-        1 2 4 이면
+        1 2   4 이면
         1 2 3 4 5 6 7  저울질 가능
         sum = 7
         answer = 8
 
-        1 2 3 이면
-        1 2 3 4 5 6    저울질 가능
-        sum = 6
-        answer : 7
+        1 2   5 이면
+        1 2 3   5 6 7  저울질 가능
+        sum = 3
+        answer : 4
         */
-
         for(int i = 0; i < n; i++){
             if(weight[i] <= sum + 1){
                 sum += weight[i];
             }
+            else break;
         }
 
         sum += 1;
