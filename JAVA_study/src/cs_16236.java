117cf5e90ea5b15b7f7cd5ff1405b8113ef7dc47
sunjinb <tjswls8062@skuniv.ac.kr>
2022-08-20 23:32:06 +0900

    220820

--- JAVA_study/src/cs_16236.java
+++ JAVA_study/src/cs_16236.java
@@ -1,5 +1,31 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class cs_16236 {
-    public static void main(String[] args) {
+    static int N;
+    static int[][] space;;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        space = new int[N][N];
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                space[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+    }
+
+    public static void bfs(int[][] space) {
 
     }
 }
