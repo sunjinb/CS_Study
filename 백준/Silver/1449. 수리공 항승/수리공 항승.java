312208807b93768804c80641a0670fc8e49ac3e3
sunjinb <tjswls8062@skuniv.ac.kr>
2022-10-20 09:12:41 +0900

    221020

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int length = sc.nextInt();
+        int[] pipe = new int[num];
+
+        for (int i = 0; i < num; i++){
+            pipe[i] = sc.nextInt();
+        }
+
+        Arrays.sort(pipe);
+
+        int tape_count = 1;
+        int start = pipe[0];
+        for(int i = 0; i < num; i++){
+            if(pipe[i] < (start + length)){
+                continue;
+            }
+            else{
+                tape_count++;
+                start = pipe[i];
+            }
+        }
+        System.out.println(tape_count);
+    }
+}
