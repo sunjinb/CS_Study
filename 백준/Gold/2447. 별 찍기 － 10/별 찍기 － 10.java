2ff0e51eb486e048012d36948edd453ed129e7bf
김선진 <tjswls8062@skuniv.ac.kr>
2022-11-03 09:05:58 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 428 ms, Memory: 50944 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    static char[][] arrays;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        arrays = new char[num][num];
+
+        drawStar(0, 0, num, false);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < num; i++){
+            for(int j = 0; j < num; j++){
+                sb.append(arrays[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static void drawStar(int x, int y, int N, boolean blank) {
+        if (blank) {
+            for (int i = x; i < x + N; i++){
+                for (int j = y; j < y + N; j++){
+                    arrays[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        if (N == 1){
+            arrays[x][y] = '*';
+            return;
+        }
+
+        int size = N / 3;
+        int count = 0;
+        for (int i = x; i < x + N; i += size){
+            for (int j = y; j < y + N; j+= size){
+                count++;
+                if(count == 5){
+                    drawStar(i, j, size, true);
+                }
+                else{
+                    drawStar(i, j, size, false);
+                }
+            }
+        }
+    }
+}
