6da1d5a1d3af6ccaed4a9732dccb774a48501b03
김선진 <tjswls8062@skuniv.ac.kr>
2022-11-08 10:10:37 +0900

    [Bronze III] Title: 오븐 시계, Time: 252 ms, Memory: 18428 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int hour, minute, oven = 0;
+        hour = sc.nextInt();
+        minute = sc.nextInt();
+        oven = sc.nextInt();
+
+        int result_h = 0, result_m = 0;
+        if(minute + oven > 59){
+            result_h = hour + ((minute + oven) / 60);
+            if(result_h > 23){
+                result_h = result_h - 24;
+            }
+            result_m = result_m + ((minute + oven) % 60);
+        }
+        else{
+            result_h = hour;
+            result_m = minute + oven;
+        }
+        System.out.println(result_h + " " + result_m);
+    }
+}
