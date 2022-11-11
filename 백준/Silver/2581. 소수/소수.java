b5d4ef1ef5640ad2a5d3871c22196931076c51b8
김선진 <tjswls8062@skuniv.ac.kr>
2022-11-12 08:59:43 +0900

    [Silver V] Title: 소수, Time: 168 ms, Memory: 14808 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        boolean flag;
+
+        for (int i = M; i <= N; i++){
+            flag = true;
+            if(i == 1){
+                flag = false;
+            }
+            for(int j = 2; j < i; j++){
+                if(i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true){
+                if(i < min){
+                    min = i;
+                }
+                sum += i;
+            }
+        }
+        if(sum == 0){
+            System.out.println(-1);
+        }
+        else{
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+} 
