dcea409d57deba39240cae947e93ac032ca09bc9
김선진 <tjswls8062@skuniv.ac.kr>
2022-11-27 21:58:55 +0900

    [Silver II] Title: 나무 자르기, Time: 544 ms, Memory: 119448 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 각각 나무의 높이 선언
+        int[] tree = new int[N];
+
+        int min = 0;
+        int max = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < tree.length; i++){
+            tree[i] = Integer.parseInt(st.nextToken());
+
+            if (max < tree[i]) {
+                max = tree[i];
+            }
+        }
+
+        // 이분 탐색
+        while(min < max){
+            int mid = (min + max) / 2;
+            long sum = 0;
+            for(int treeHeight : tree){
+                if(treeHeight - mid > 0){
+                    sum += (treeHeight - mid);
+                }
+            }
+
+            if(sum < M){
+                max = mid;
+            }
+
+            else{
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+    }
+}
