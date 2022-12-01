91722441c698512141c7d2e944259d03da25fb64
김선진 <tjswls8062@skuniv.ac.kr>
2022-12-01 09:00:33 +0900

    [Bronze II] Title: 상수, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+        System.out.println(A > B ? A : B);
+
+    }
+}
