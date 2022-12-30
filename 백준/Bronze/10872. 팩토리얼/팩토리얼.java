57c1dd4ee9d0d7721e9a893708c8d1a9d7833873
김선진 <tjswls8062@skuniv.ac.kr>
2022-12-30 09:51:10 +0900

    [Bronze V] Title: 팩토리얼, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        
+        int sum = factorial(N);
+        System.out.println(sum);
+    }
+    
+    public static int factorial(int N){
+        if(N < 1){
+            return 1;
+        }
+        return N * factorial(N - 1);
+    }
+}
