efd63da847b4fc7eed4021767a93379b6ada9264
김선진 <tjswls8062@skuniv.ac.kr>
2022-12-31 18:35:38 +0900

    [Bronze V] Title: ??!, Time: 220 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String id = new String();
+        Scanner sc = new Scanner(System.in);
+        id = sc.nextLine();
+        System.out.println(id + "??!");
+    }
+}
