44d8056eb4a93a672b0726f855d8e8506b6ad6e1
김선진 <tjswls8062@skuniv.ac.kr>
2023-02-27 22:38:45 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 344 ms, Memory: 16412 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = N; i > 0; i--) {
+			for(int j = 0; j < N - i; j++) {
+				System.out.print(' ');
+			}
+			for(int j = 0; j < i; j++) {
+				System.out.print('*');
+			}
+
+			System.out.println();
+		}
+	}
+}
