2d4ce288bd535757468afcf0bad9de0f36e8ab42
sunjinb <tjswls8062@skuniv.ac.kr>
2023-03-04 18:38:19 +0900

    update

--- JAVA_study/src/cs_s5432.java
+++ JAVA_study/src/cs_s5432.java
@@ -1,5 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
 public class cs_s5432 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            String str = br.readLine();
+        }
     }
 }
