a082c67b6c433a308aa7439524f7bd982f5db3b2
김선진 <tjswls8062@skuniv.ac.kr>
2023-04-16 16:13:19 +0900

    [Gold III] Title: 웜홀, Time: 444 ms, Memory: 22536 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main {
     static int N, M, W;
-    static final int INF = Integer.MAX_VALUE;
+    static final int INF = 987654321;
     static int[] distance;
     static ArrayList<Edge> list;
     static class Edge{
@@ -18,6 +18,7 @@ public class Main {
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
@@ -47,32 +48,25 @@ public class Main {
                 list.add(new Edge(from, to, -cost));
             }
 
-            boolean flag = false;
-            for(int start = 1; start <= N; start++){
-                if(BellmanFord(start)){
-                    flag = true;
-                    break;
-                }
-            }
-
-            if(flag) System.out.println("YES");
-            else System.out.println("NO");
+            if(BellmanFord()) sb.append(("YES\n"));
+            else sb.append("NO\n");
         }
+        System.out.println(sb);
     }
 
     // 그냥 음의 사이클이 발생하는지 안 하는지 여부만 판단하면 됨
-    private static boolean BellmanFord(int start) {
+    private static boolean BellmanFord() {
         distance = new int[N + 1];
         Arrays.fill(distance, INF);
-        distance[start] = 0;
+        distance[1] = 0;
 
         // 시작점부터 이어지는 모든 연결된 부분들까지의 거리 최소 구해놓기
-        for(int i = 1; i <= N; i++){
+        for(int i = 1; i < N; i++){
             boolean flag = false;
             for(int j = 0; j < list.size(); j++){
                 Edge edge = list.get(j);
 
-                if(distance[edge.from] != INF && distance[edge.to] > distance[edge.from] + edge.cost){
+                if(distance[edge.to] > distance[edge.from] + edge.cost){
                     distance[edge.to] = distance[edge.from] + edge.cost;
                     flag = true;
                 }
@@ -82,7 +76,7 @@ public class Main {
 
         for(int i = 0; i < list.size(); i++){
             Edge edge = list.get(i);
-            if(distance[edge.from] != INF && distance[edge.to] > distance[edge.from] + edge.cost){
+            if(distance[edge.to] > distance[edge.from] + edge.cost){
                 return true;
             }
         }
