edc8b7ad8f6cb4e947da2cbb6f7f87df82778372
김선진 <tjswls8062@skuniv.ac.kr>
2023-05-07 13:56:37 +0900

    [Gold IV] Title: 부분합, Time: 308 ms, Memory: 24136 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, S;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int onePointer = 0;
+        int twoPointer = 0;
+        int sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        while(twoPointer <= N){
+            // 현재까지 합이 S이상일 경우
+            if(S <= sum){
+                sum -= arr[onePointer];
+                onePointer++;
+                int tmpLength = twoPointer - onePointer + 1;
+                minLength = Math.min(tmpLength, minLength);
+            }
+
+            // 조건 충족 안되는 경우
+            else{
+                sum += arr[twoPointer];
+                twoPointer++;
+            }
+        }
+
+        if(minLength == Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println(minLength);
+    }
+}
