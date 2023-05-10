4c191c7cdcf6aad7e3a801a67497118ece656600
김선진 <tjswls8062@skuniv.ac.kr>
2023-05-10 09:15:37 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -20,26 +20,11 @@ public class Main {
 			}
 			
 			int mid = arr.length / 2;
-			int mod = arr.length % 2;
 			
-			// 짝수 자리 수라면
-			if(mod == 0) {
-//				4 / 2 = 2 중앙 + 1
-				for(int i = 0; i < mid; i++) {
-					if(arr[i] != arr[arr.length - i - 1]) {
-						isPelindrom = false;
-						break;
-					}
-				}
-			}
-			// 홀수 자리 수라면
-			else if(mod == 1) {
-//				5 / 2 = 2 중앙
-				for(int i = 0; i < mid; i++) {
-					if(arr[i] != arr[arr.length - i - 1]) {
-						isPelindrom = false;
-						break;
-					}
+			for(int i = 0; i < mid; i++) {
+			if(arr[i] != arr[arr.length - i - 1]) {
+				isPelindrom = false;
+				break;
 				}
 			}
 			
