f30d63654a75cba65221fe18f3d5a6fe64fa1aa1
김선진 <tjswls8062@skuniv.ac.kr>
2023-06-07 13:45:50 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 784 ms, Memory: 529632 KB -BaekjoonHub

---
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main{
+    static char[][] map;
+    static class Point{
+        int row, col, time;
+        public Point(int row, int col, int time){
+            this.row = row;
+            this.col = col;
+            this.time = time;
+        }
+    }
+    static int[] dRow = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+    static int[] dCol = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        map = new char[8][8];
+
+        for(int i = 0; i < 8; i++){
+            String str = br.readLine();
+            for(int j = 0; j < 8; j++){
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(7, 0, 0));
+
+        boolean flag = false;
+        
+        // 시간 돌리는거
+        while(true){
+            if(q.isEmpty()) break;
+            
+            int qSize = q.size();
+            
+            while(qSize > 0){
+                qSize--;
+
+                Point now = q.poll();
+
+                if(map[now.row][now.col] == '#'){
+                    continue;
+                }
+
+                if(now.row == 0 && now.col == 7){
+                    flag = true;
+                    break;
+                }
+
+                for(int i = 0; i < 9; i++){
+                    int nextRow = now.row + dRow[i];
+                    int nextCol = now.col + dCol[i];
+
+                    if(nextRow < 0 || nextCol < 0 || nextRow > 7 || nextCol > 7) continue;
+
+                    if(map[nextRow][nextCol] == '.'){
+                        q.add(new Point(nextRow, nextCol, now.time + 1));
+                    }
+                }
+            }
+            
+            if(flag) break;
+            moveWall();
+        }
+
+        
+
+        if(!flag) System.out.println(0);
+        else System.out.println(1);
+    }
+
+    private static void moveWall() {
+        for(int i = 7; i > 0; i--){
+            for(int j = 0; j < 8; j++){
+                map[i][j] = map[i - 1][j];
+            }
+        }
+
+        // 맨 위 벽 처리
+        for(int i = 0; i < 8; i++){
+            map[0][i] = '.';
+        }
+    }
+}
